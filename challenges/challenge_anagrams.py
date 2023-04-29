470ae0f818f99d6217e2aec4ae6934972530822e
Lucas Ximenes <lucasdximenes@gmail.com>
2023-04-29 18:54:02 -0300

    perf: :zap: change `is_anagram` sort function

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,15 +1,38 @@
-def bubble_sort(str):
-    n = len(str)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if str[j] > str[j+1]:
-                str = str[:j] + str[j+1] + str[j] + str[j+2:]
-    return str
+def merge(left, right):
+    res = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            res.append(left[i])
+            i += 1
+        else:
+            res.append(right[j])
+            j += 1
+
+    res += left[i:]
+    res += right[j:]
+
+    return res
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    return merge(left, right)
 
 
 def is_anagram(first_string, second_string):
-    sorted_first_string = bubble_sort(first_string.lower())
-    sorted_second_string = bubble_sort(second_string.lower())
+    sorted_first_string = "".join(merge_sort(first_string.lower()))
+    sorted_second_string = "".join(merge_sort(second_string.lower()))
 
     if first_string == "" or second_string == "":
         return (sorted_first_string, sorted_second_string, False)
