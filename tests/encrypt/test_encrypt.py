557f551adb19e500cf01fa8a3338d1ee8fe655ed
Lucas Ximenes <lucasdximenes@gmail.com>
2023-04-29 17:57:23 -0300

    test: :white_check_mark: add `encrypt_message` test

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,11 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(1, 1)
+
+    assert encrypt_message("abc", 3) == "cba"
+    assert encrypt_message("haikyuu!", 3) == "iah_!uuyk"
+    assert encrypt_message("haikyuu!", 4) == "!uuy_kiah"
