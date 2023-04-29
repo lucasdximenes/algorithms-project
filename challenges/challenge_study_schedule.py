f37c6dbe594c0885704aa56821fc3fb86785fd0b
Lucas Ximenes <lucasdximenes@gmail.com>
2023-04-29 17:40:36 -0300

    feat: :rocket: implement `study_schedule` function

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    student_quantity = 0
+    for period in permanence_period:
+        if not (isinstance(period[0], int) and isinstance(period[1], int)):
+            return None
+        if period[0] <= target_time <= period[1]:
+            student_quantity += 1
+
+    return student_quantity
